e3de96e2c88e02810b63f77d532f5eecd0eb18b9
Joe <56809242+Jo3-L@users.noreply.github.com>
2021-07-06 16:06:55 -0700

    main: fix problem matcher registration

--- main.go
+++ main.go
@@ -4,8 +4,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 	"strings"
 
@@ -15,16 +17,41 @@ import (
 func checkFile(path string) error {
 	contents, err := os.ReadFile(path)
 	if err != nil {
-		log.Fatal("failed reading file: ", err)
+		log.Fatal("error reading file: ", err)
 	}
 
 	_, err = template.New("").Funcs(funcs).Parse(string(contents))
 	return err
 }
 
+func registerProblemMatcher() {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		log.Fatal("error getting user home dir: ", err)
+	}
+
+	in, err := os.Open("/check_yag_tmpl_syntax.json")
+	if err != nil {
+		log.Fatal("error reading syntax matcher file: ", err)
+	}
+	defer in.Close()
+
+	dst := path.Join(dirname, "check_yag_tmpl_syntax.json")
+	out, err := os.Create(dst)
+	if err != nil {
+		log.Fatal("error creating file under user home dir: ", err)
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, in)
+	if err != nil {
+		log.Fatal("error copying problem matcher to user home dir: ", err)
+	}
+	fmt.Println("::add-matcher::" + dst)
+}
+
 func main() {
-	// register the problem matcher
-	fmt.Println("##[add-matcher].check_yag_tmpl_syntax.json")
+	registerProblemMatcher()
 
 	matches, err := filepath.Glob(os.Getenv("INPUT_INCLUDE"))
 	if err != nil {
@@ -50,7 +77,7 @@ func main() {
 	out = strings.ReplaceAll(out, "%", "%25")
 	out = strings.ReplaceAll(out, "\n", "%0A")
 	out = strings.ReplaceAll(out, "\r", "%0D")
-	fmt.Println("##set-output name=output::", out)
+	fmt.Println("::set-output name=output::" + out)
 
 	if out == "" {
 		os.Exit(0)
